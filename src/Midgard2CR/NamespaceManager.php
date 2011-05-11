f01d67347c89d37b17191bc1b47a3b7d7b3ec706
piotras <piotrek.pokora@gmail.com>
2011-05-11 11:17:28 +0200

    Namespaces and prefixes helper

--- src/Midgard2CR/NamespaceManager.php
+++ src/Midgard2CR/NamespaceManager.php
@@ -3,12 +3,19 @@ namespace Midgard2CR;
 
 class NamespaceManager 
 {
-    protected $session = null;
+    protected $registery = null;
 
-    public function __construct(\Midgard2CR\Session $session)
+    public function __construct(\Midgard2CR\NamespaceRegistry $registry)
     {
-        $this->session = $session;    
-        $this->registry = $this->builtins;
+        $this->registry = $registry
+    }
+
+    /**
+     * Get NamespaceRegistry object associated with manager
+     */
+    public function getRegistry()
+    {
+        return $this->registry;
     }
 
     /**
@@ -16,31 +23,81 @@ class NamespaceManager
      */
     public function isPrefix($string)
     {
-
+        $prefix = $this->registry->getPrefix();
+        if ($prefix == false)
+        {
+            return false;
+        }
+        return true;
     }
 
     /**
      * From given string get known prefix.
+     *
+     * Return 'mgd' from:
+     * 'mgd:Person'
+     * 'http://www.midgard-project.org/repligard/1.4/Person'
      */ 
     public function getPrefix($string)
     {
-
+        $tokens = $this->getPrefixTokens($string);
+        if ($tokens[0] != null)
+            return $tokens[0];
+        return null;
     }
 
     /**
      * From given string get prefix tokens.
+     *
+     * Return array('mgd', 'Person') from:
+     * 'mgd:Person'
+     * 'http://www.midgard-project.org/repligard/1.4/Person'
      */ 
     public function getPrefixTokens($string)
     {
+        $tokens = array (null, null);
 
+        /* Registered prefix */
+        if ($this->isPrefix($string))
+        {
+            $tokens[0] = $string;
+            return $tokens;
+        }
+  
+        /* Given string is uri */
+        if (substr_count($string, '/') > 0) {
+            $uri_tokens = $this->getUriTokens($string);
+            if ($uri_tokens[0] != null) {
+                $tokens[0] = $this->registry->getPrefix($uri_tokens[0]);
+                $tokens[1] = $uri_tokens[1];
+            }
+            return $tokens;
+        }
+
+        /* Given string is prefix statement */
+        if (substr_count($string, ':') > 0) 
+        {
+            $spltd = explode($string, ':');
+            if ($this->isPrefix($spltd[0]) == true) {
+                $tokens[0] = $spltd[0];
+                $tokens[1] = $spltd[1];
+            }
+        }
+        return $tokens;
     }
 
     /**
-     * From given string get prefix with statement 
-     * */
+     * From given string get prefix with statement.
+     *
+     * Return 'mgd:Person' from
+     * 'http://www.midgard-project.org/repligard/1.4/Person'
+     */
     public function getPrefixWithStatement($string)
     {
-
+        $tokens = $this->getPrefixTokens($string);
+        if ($tokens[0] != null && $tokens[1] != null)
+            return $tokens[0] . ":" . $tokens[1];
+        return null;
     }
 
     /** 
@@ -48,7 +105,10 @@ class NamespaceManager
      */ 
     public function isUri($string)
     {
-
+        $uri = $this->registry->getUri($string);
+        if ($uri == false)
+            return false;
+        return true;
     }
 
     /** 
@@ -56,7 +116,10 @@ class NamespaceManager
      */ 
     public function getUri($string)
     {
-
+        $tokens = $this->getUriTokens($string);
+        if ($tokens[0] == null)
+            return null;
+        return $tokens[0];
     }
 
     /**
@@ -64,7 +127,45 @@ class NamespaceManager
      */ 
     public function getUriTokens($string)
     {
+        $tokens = array(null, null);
 
+        /* Given string is uri */
+        if ($this->isUri($string))
+        {
+            $tokens[0] = $string;
+            return $tokens;
+        }
+
+        /* Assume, given string is '#' terminating */
+        if (substr_count($string, '#', 1) {
+            $spltd = explode($string, '#');
+            if ($spltd[0] != null)
+                if ($this->isUri($spltd[0] . '#')) {
+                    $tokens[0] = $spltd[0] . '#';
+                    $tokens[1] = $spltd[1];
+                }
+            return $tokens;
+        }
+        
+        /* prefix or unknown string, try to get prefix */
+        if (substr_count($string, '/') == 0) {
+            $prefix_tokens = $this->getPrefixTokens($string);
+            if ($prefix_tokens[0] != null) {
+                $tokens[0] = $this->getUri($prefix_tokens[0]);
+                $tokens[1] = $prefix_tokens[1];
+            }
+            return $tokens;
+        }
+        
+        /* check uri */
+        $rs = strrch ($string, '/');
+        $uri = substr($string, 0, (strlen($string) - strlen($rs)) + 1);
+        if ($this->isUri($uri) == true) {
+            $tokens[0] = $uri;
+            $tokens[1] = substr($rs, 1, -1);
+        }
+
+        return $tokens;
     }
 
     /**
@@ -72,7 +173,10 @@ class NamespaceManager
      */ 
     public function getUriWithStatement($string)
     {
-
+        $tokens = $this->getUriTokens($string);
+        if ($tokens[0] != null && $tokens[1] != null)
+            return $tokens[0] . $tokens[1];
+        return null;
     }
 }
 
