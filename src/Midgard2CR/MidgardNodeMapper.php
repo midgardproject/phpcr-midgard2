1a2ed654411b26a601a48c0a478e73a728481a6f
piotras <piotrek.pokora@gmail.com>
2011-07-18 12:35:57 +0200

    Initial, maps PHPCR and Midgard2 node names

---
+++ src/Midgard2CR/MidgardNodeMapper.php
@@ -0,0 +1,40 @@
+<?php
+
+class MidgardNodeMapper 
+{
+    /**
+     * Replaces ':' with '_' and returns given type converted to lowercase.
+     */ 
+    public static function getMidgardName($type)
+    {
+        if (strpos($type, ':') === false)
+        {
+            if (strpos($type, '_') !== false)
+            {
+                return $type;
+            }
+            return null;
+        }
+
+        return strtolower(str_replace(':', '_', $type));
+    }
+
+    /**
+     * Replaces '_' with ':' in given type 
+     */ 
+    public static function getPHPCRName($type)
+    {
+        if (strpos($type, '_') === false)
+        {
+            if (strpos($type, ':') !== false)
+            {
+                return $type;
+            }
+            return null;
+        }
+        /* TODO, determine uper cases */
+        return str_replace('_', ':', $type);
+    }
+}
+
+?>
