917bc714a731a0bd768fca903c0a538a536af83b
piotras <piotrek.pokora@gmail.com>
2011-07-29 14:25:06 +0200

    Add namespaces

--- src/Midgard2CR/XMLExporter.php
+++ src/Midgard2CR/XMLExporter.php
@@ -5,12 +5,13 @@ namespace Midgard2CR;
 abstract class XMLExporter
 {
     protected $xmlDoc = null;
-    protected $xmlWriter = null;
     protected $session = null;
     protected $svNS = 'sv';
     protected $svUri = null;
     protected $xmlNode = null;
+    protected $xmlRootNode = null;
     protected $node = null;
+    protected $nsRegistry = null;
 
     public function serializeProperties(Node $node, DOMNode $xmlNode, $skipBinary)
     {
@@ -22,9 +23,41 @@ abstract class XMLExporter
 
     }
 
+    protected function addNamespaceAttribute($name)
+    {
+        if (!$this->xmlRootNode)
+        {
+            return;
+        }
+
+        $nsManager = $this->nsRegistry->getNamespaceManager();
+        $prefix = $nsManager->getPrefix($name);
+
+        if (!$prefix)
+        {
+            return;
+        }
+
+        $uri = $this->nsRegistry->getURI($prefix);
+        if (!$uri)
+        {
+            return;
+        }
+
+        try {
+            $nsAttr = $this->xmlDoc->createAttribute('xmlns:'.$prefix);
+            $nsAttr->value = $uri;
+            $this->xmlRootNode->appendChild($nsAttr);
+        }
+        catch (\DOMException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+
     public function getXMLBuffer()
     {
-        return $this->xmlDoc->saveXML($this->xmlNode);
+        return $this->xmlDoc->saveXML();
     }
 
     public function serializeGraph()
