578355ff11c9993a0d8afce7eca5cc5571e66d38
Henri Bergius <henri.bergius@iki.fi>
2011-08-03 16:57:53 +0300

    Fix factory interface

--- src/Midgard2CR/RepositoryFactory.php
+++ src/Midgard2CR/RepositoryFactory.php
@@ -3,16 +3,13 @@ namespace Midgard2CR;
 
 class RepositoryFactory implements \PHPCR\RepositoryFactoryInterface
 {
-    public function __construct()
+    public static function getRepository(array $parameters = NULL)
     {
         if (!extension_loaded('midgard2'))
         {
-            throw new \PHPCR\RepositoryException();
+            throw new \PHPCR\RepositoryException("The Midgard2 PHPCR provider requires 'midgard2' extension to be loaded.");
         }
-    }
 
-    public static function getRepository(array $parameters = NULL)
-    {
         return new Repository();    
     }
 
