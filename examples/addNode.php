49783427d2731efea10661c01b0f2379ccaa3db3
Henri Bergius <henri.bergius@iki.fi>
2011-08-04 01:29:13 +0300

    Provide a simple node adding example

---
+++ examples/addNode.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This example demonstrates how to add and access nodes in a
+ * PHPCR tree.
+ *
+ * The basic code comes from the PHPCR tutorial:
+ * https://github.com/phpcr/phpcr/blob/master/doc/Tutorial.md 
+ */
+
+// Set up autoloader
+require __DIR__ . "/includes.php";
+
+// Set up Midgard2 repository configs
+$parameters = array(
+    // Use local SQLite file for storage
+    'midgard2.configuration.db.type' => 'SQLite',
+    'midgard2.configuration.db.name' => 'midgard2cr',
+    'midgard2.configuration.db.dir' => __DIR__,
+    // Let Midgard2 initialize the DB as needed
+    'midgard2.configuration.db.init' => true,
+    // Enable this if you want to see the actual database queries
+    //'midgard2.configuration.loglevel' => 'debug',
+);
+
+// Get a Midgard repository
+$repository = Midgard2CR\RepositoryFactory::getRepository($parameters);
+
+// Log in
+$credentials = new \PHPCR\SimpleCredentials('admin', 'password');
+$session = $repository->login($credentials, 'default');
+
+// Add node unless it already exists
+if (!$session->nodeExists('/test')) {
+    $root = $session->getRootNode();
+    $node = $root->addNode('test', 'nt:unstructured');
+    $node->setProperty('prop', 'value');
+    $session->save();
+}
+// Read the values we just saved
+$node = $session->getNode('/test');
+var_dump($node->getPropertyValue('prop'));
