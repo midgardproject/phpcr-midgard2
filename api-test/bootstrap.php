01a6bc97b6e3fda32cd3db5bbea8b1434496d2e3
Henri Bergius <henri.bergius@iki.fi>
2011-08-03 14:15:28 +0300

    Disable PHP Garbage Collection to avoid segfaults, see https://github.com/midgardproject/midgard-php5/issues/50

--- api-test/bootstrap.php
+++ api-test/bootstrap.php
@@ -4,6 +4,13 @@ if (!extension_loaded('xdebug') && ini_get('enable_dl')) {
     dl('xdebug.so');
 }
 
+if (gc_enabled()) {
+    echo "Disabling Zend Garbage Collection to prevent segfaults, see:\n";
+    echo "  https://bugs.php.net/bug.php?id=51091\n";
+    echo "  https://github.com/midgardproject/midgard-php5/issues/50\n";
+    gc_disable(); 
+}
+
 require_once(dirname(__FILE__) . '/../SplClassLoader.php');
 
 // Midgard2CR is in the src dir
