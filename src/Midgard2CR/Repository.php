0b10b6afeec396eebcc5c945099ed9abdd3f4006
piotras <piotrek.pokora@gmail.com>
2011-08-04 21:35:45 +0200

    credentials are optional

--- src/Midgard2CR/Repository.php
+++ src/Midgard2CR/Repository.php
@@ -58,13 +58,13 @@ class Repository implements \PHPCR\RepositoryInterface
         return $mgd;
     }
     
-    private function midgard2Login($credentials)
+    private function midgard2Login($credentials = null)
     {
         // TODO: Handle different authtypes
         $tokens = array
         (
-            'login' => $credentials->getUserID(),
-            'password' => $credentials->getPassword(),
+            'login' => $credentials ? $credentials->getUserID() : 'admin',
+            'password' => $credentials ? $credentials->getPassword() : 'password',
             'authtype' => 'Plaintext',
             'active' => true
         );
