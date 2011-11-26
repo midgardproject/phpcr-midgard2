9d192d553edda64a9c08dead710cdbb20429f0d9
piotras <piotrek.pokora@gmail.com>
2011-11-26 21:42:44 +0100

    Remove tests from ignored ones

--- api-test/ImplementationLoader.php
+++ api-test/ImplementationLoader.php
@@ -13,10 +13,7 @@ class ImplementationLoader extends \PHPCR\Test\AbstractLoader
     );
 
     protected $unsupportedTests = array(
-        'Query\QOM\Sql2ToQomConverterTest::testColumnsAndSelector',
         'Connecting\WorkspaceReadMethodsTest::testGetAccessibleWorkspaceNames',
-        'Query\QOM\Sql2ToQomConverterTest::testQueries',
-        'Query\QOM\ConvertQueriesBackAndForthTest::testBackAndForth',
         'Reading\SessionReadMethodsTest::testImpersonate',
         'Reading\SessionReadMethodsTest::testCheckPermission',
         'Reading\SessionReadMethodsTest::testCheckPermissionAccessControlException',
