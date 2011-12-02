fd57387af3355a4fa7e01f340dc09b6fc67bfff1
piotras <piotrek.pokora@gmail.com>
2011-12-02 12:43:56 +0100

    Renamed to avoid autoloader issues

---
+++ src/Midgard/PHPCR/Query/QOM/_And.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Midgard\PHPCR\Query\QOM;
+
+/**
+ * {@inheritDoc}
+ */
+class _And implements \PHPCR\Query\QOM\AndInterface
+{
+    protected $constraintFirst = null;
+    protected $constraintSecond = null;
+
+    public function __construct($constraintFirst, $constraintSecond)
+    {
+        $this->constraintFirst = $constraintFirst;
+        $this->constraintSecond = $constraintSecond;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getConstraint1()
+    {
+        return $this->constraintFirst;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getConstraint2()
+    {
+        return $this->constraintSecond;
+    }
+}
