bef6f79213e989380b5cefa4e3f0634f47a622b7
piotras <piotrek.pokora@gmail.com>
2011-12-02 16:12:29 +0100

    Initial to create implicit joins for orderings and ChildNode constraints

--- src/Midgard/PHPCR/Query/SQLQuery.php
+++ src/Midgard/PHPCR/Query/SQLQuery.php
@@ -11,6 +11,9 @@ class SQLQuery implements \PHPCR\Query\QueryInterface
     protected $selectors = array();
     protected $converter = null;
     protected $query = null;
+    protected $propertyStorage = null;
+    protected $defaultNodeStorage = null;
+    protected $defaultGroupConstraint = null;
 
     protected $storageType = null;
     
@@ -20,8 +23,51 @@ class SQLQuery implements \PHPCR\Query\QueryInterface
         $this->statement = $statement;
         $this->converter = new \PHPCR\Util\QOM\Sql2ToQomQueryConverter(new QOM\QueryObjectModelFactory());
         $this->query = $this->converter->parse($statement);
-        print_r($this->query);
         $this->QBFromStatement();
+    }
+
+    private function getDefaultNodeStorage()
+    {
+        if ($this->defaultNodeStorage == null)
+            $this->defaultNodeStorage = new \midgard_query_storage('midgard_node');
+        return $this->defaultNodeStorage;
+    }
+
+    private function getQuerySelect()
+    {
+        if ($this->qs == null) 
+            $this->qs = new \midgard_query_select($this->getDefaultNodeStorage());
+        return $this->qs;
+    }
+
+    private function getPropertyStorage()
+    {
+        if ($this->propertyStorage == null)
+            $this->propertyStorage = new \midgard_query_storage('midgard_node_property');
+        return $this->propertyStorage;
+    }
+
+    private function getDefaultGroupConstraint()
+    {
+        if ($this->defaultGroupConstraint == null) 
+            $this->defaultGroupConstraint = new \midgard_query_constraint_group("AND");
+        return $this->defaultGroupConstraint;
+    }
+
+    /* Add implicit join.
+     * We join midgard_node_property.parent on midgard_node.id */
+    private function addJoinIDToParent()
+    {
+        static $joined = false;
+        if ($joined == true)
+            return;
+
+        $this->getQuerySelect()->add_join(
+            'INNER',
+            new \midgard_query_property('id'),
+            new \midgard_query_property('parent', $this->getPropertyStorage())
+        );
+        $joined = true;
     }
 
     private function addConstraintSingle(\midgard_query_constraint $constraint)
@@ -38,6 +84,61 @@ class SQLQuery implements \PHPCR\Query\QueryInterface
             $cg->add_constraint($constraint);
         }
         $this->qs->set_constraint($cg);
+    }
+
+    private function addConstraintChildNode()
+    {
+        /* There's a path specified so we need to find node under parent's path */
+        $parentPath = $this->query->getConstraint()->getParentPath();
+        $parts = explode("/", $parentPath);
+        if (empty($parts))
+            return;
+        /* Reverse path's elements so we can generate correct joins */
+        $parts = array_reverse($parts);
+        $currentStorage = null;
+        /* For each element in path, add join and constraint with parent's name */
+        foreach ($parts as $name) {
+            $nodeStorage = new \midgard_query_storage("midgard_node");
+            $this->getQuerySelect()->add_join(
+                'INNER',
+                $currentStorage == null ? new \midgard_query_property('parent') : new \midgard_query_property('parent', $currentStorage),
+                new \midgard_query_property('id', $nodeStorage)
+            );
+            $name = str_replace('"', '', $name);
+            $constraint = new \midgard_query_constraint(
+                new \midgard_query_property("name", $nodeStorage),
+                "=",
+                new \midgard_query_value ($name == '' ? 'root' : $name)
+            );
+            $this->getDefaultGroupConstraint()->add_constraint($constraint);
+            $currentStorage = $nodeStorage;
+        } 
+    }
+
+    private function addOrders()
+    {
+        $orderings = $this->query->getOrderings();
+        if (empty($orderings))
+            return;
+
+        //$this->addJoinIDToParent();
+
+        foreach ($orderings as $order) {
+            print_r($order->getOperand()->getPropertyName());
+            $constraint = new \midgard_query_constraint (
+                new \midgard_query_property ("value"),
+                "=",
+                new \midgard_query_value ($order->getOperand()->getPropertyName()),
+                $this->getPropertyStorage()
+            );
+            $propertyStorage = new \midgard_query_storage ("midgard_node_property");
+            $this->getQuerySelect()->add_join(
+                'INNER',
+                new \midgard_query_property('id'),
+                new \midgard_query_property('parent', $propertyStorage)
+            );
+            $this->getQuerySelect()->add_order (new \midgard_query_property('value', $propertyStorage));
+        }
     }
 
     private function QBFromStatement()
@@ -84,10 +185,22 @@ class SQLQuery implements \PHPCR\Query\QueryInterface
             );
         }
 
-        if (count($constraints) > 1) {
+        /*if (count($constraints) > 1) {
             return $this->addConstraintMultiple($constraints);
         }
-        $this->addConstraintSingle($constraints[0]);
+        $this->addConstraintSingle($constraints[0]);*/
+        if (is_a($this->query->getConstraint(), 'Midgard\PHPCR\Query\QOM\ChildNode'))
+            $this->addConstraintChildNode(); 
+
+        echo "\n\n EXECUTE : " . $this->statement . "\n";
+
+        $this->addOrders();
+
+        \midgard_connection::get_instance()->set_loglevel("debug");
+        $this->qs->set_constraint($this->getDefaultGroupCOnstraint());
+        $this->qs->execute();
+        \midgard_connection::get_instance()->set_loglevel("warn");
+        die();
     } 
 
     public function bindValue($varName, $value)
