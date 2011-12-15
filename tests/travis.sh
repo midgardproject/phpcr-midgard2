75ba6bd92af351b95c92a601f99d3ab8ff4c6616
Henri Bergius <henri.bergius@iki.fi>
2011-12-15 16:16:47 +0200

    Copy views as well

--- tests/travis.sh
+++ tests/travis.sh
@@ -7,6 +7,7 @@ git submodule update
 
 # Copy Midgard2 PHPCR schemas
 sudo cp -r ./data/share/schema/* /usr/share/midgard2/schema/
+sudo cp -r ./data/share/views/* /usr/share/midgard2/views/
 
 # Install dependencies with Composer
 wget -q http://getcomposer.org/composer.phar
