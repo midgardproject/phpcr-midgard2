5cc5e0cea005d17eae643d03cb5e38969dd668ba
piotras <piotrek.pokora@gmail.com>
2011-12-29 14:26:34 +0100

    Remove ratatoskr directory, silently unack core tarball

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -12,9 +12,10 @@ MIDGARD_EXT_VERSION=ratatoskr
 sudo apt-get install -y dbus libglib2.0-dev libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev valgrind 
 
 # Build Midgard2 core from recent tarball
-wget -q https://github.com/midgardproject/midgard-core/tarball/ratatoskr
-tar -xzvf ratatoskr
+wget -q https://github.com/midgardproject/midgard-core/tarball/${MIDGARD_EXT_VERSION}
+tar -xzf ratatoskr
 sh -c "cd midgardproject-midgard-core-*&&./autogen.sh --prefix=/usr; make; sudo make install"
+rm -f ${MIDGARD_EXT_VERSION}
 
 # Build and install Midgard2 PHP extension
 wget -q https://github.com/midgardproject/midgard-php5/tarball/${MIDGARD_EXT_VERSION}
