a112822323a1b7df97d593c9500b88c97af552e2
piotras <piotrek.pokora@gmail.com>
2012-04-05 16:21:44 +0200

    PropertyDefinitionTemplate corrections

--- src/Midgard/PHPCR/NodeType/NodeType.php
+++ src/Midgard/PHPCR/NodeType/NodeType.php
@@ -25,8 +25,7 @@ class NodeType extends NodeTypeDefinition implements NodeTypeInterface
         $this->hasOrderableChildNodes = $ntt->hasOrderableChildNodes();
         $this->isAbstract = $ntt->isAbstract();
         $this->isMixin = $ntt->isMixin();
-        $this->isQueryable = $ntt->isQueryable();
-        $this->propertyDefinitions = $ntt->getDeclaredPropertyDefinitions();
+        $this->isQueryable = $ntt->isQueryable(); 
     }
 
     public function getSupertypes()
--- src/Midgard/PHPCR/NodeType/NodeTypeTemplate.php
+++ src/Midgard/PHPCR/NodeType/NodeTypeTemplate.php
@@ -7,6 +7,8 @@ use \ArrayObject;
 
 class NodeTypeTemplate extends NodeTypeDefinition implements NodeTypeTemplateInterface
 {
+    protected $propertyDefinitionTemplates = null;
+
     public function __construct(NodeTypeDefinitionInterface $ntt = null, NodeTypeManager $manager)
     {
         if (!$ntt) {
@@ -15,11 +17,6 @@ class NodeTypeTemplate extends NodeTypeDefinition implements NodeTypeTemplateInt
 
         parent::__construct($ntt->getName(), $manager);
         $this->childNodeDefinitions = $ntt->getDeclaredChildNodeDefinitions();
-        $propdefs = $ntt->getDeclaredPropertyDefinitions();
-        if ($propdefs == null) {
-            $propdefs = array();
-        }
-        $this->propertyDefinitions = new ArrayObject($propdefs);
         $this->supertypeNames = $ntt->getDeclaredSuperTypeNames();
         $this->primaryItemName = $ntt->getPrimaryItemName();
         $this->hasOrderableChildNodes = $ntt->hasOrderableChildNodes();
@@ -35,7 +32,10 @@ class NodeTypeTemplate extends NodeTypeDefinition implements NodeTypeTemplateInt
 
     public function getPropertyDefinitionTemplates()
     {
-        return $this->propertyDefinitions;
+        if ($this->propertyDefinitionTemplates == null) {
+            $this->propertyDefinitionTemplates = new ArrayObject();
+        }
+        return $this->propertyDefinitionTemplates;
     }
 
     public function setAbstract($toggle)
