04d71cdd0a99514be3030ffaa34a6258f0c08cd8
Henri Bergius <henri.bergius@iki.fi>
2012-11-12 11:29:36 +0100

    Test against both Ratatoskr and Gjallarhorn

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -4,10 +4,6 @@
 pyrus channel-discover pear.indeyets.ru
 pyrus install -f http://pear.indeyets.ru/get/pake-1.6.3.tgz
 
-# Options
-MIDGARD_LIBS_VERSION=10.05.6
-MIDGARD_EXT_VERSION=ratatoskr
-
 # Install Midgard2 library dependencies from OBS
 sudo apt-get install -y dbus libglib2.0-dev libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev 
 
@@ -22,4 +18,3 @@ wget -q https://github.com/midgardproject/midgard-php5/tarball/${MIDGARD_EXT_VER
 tar zxf ${MIDGARD_EXT_VERSION}
 sh -c "cd midgardproject-midgard-php5-*&&php `pyrus get php_dir|tail -1`/pake.php install"
 echo "extension=midgard2.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-
