75c28c8158b3a9edc09cc764d2ce47583f57ecb4
piotras <piotrek.pokora@gmail.com>
2012-11-23 13:13:42 +0100

    Download explicitly named tarball

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -8,13 +8,13 @@ pyrus install -f http://pear.indeyets.ru/get/pake-1.6.3.tgz
 sudo apt-get install -y dbus libglib2.0-dev libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev 
 
 # Build Midgard2 core from recent tarball
-wget -q https://github.com/midgardproject/midgard-core/tarball/${MIDGARD_EXT_VERSION}
+wget -q https://github.com/midgardproject/midgard-core/tarball/${MIDGARD_EXT_VERSION} -O ${MIDGARD_EXT_VERSION}
 tar -xzf ${MIDGARD_EXT_VERSION}
 sh -c "cd midgardproject-midgard-core-*&&./autogen.sh --prefix=/usr; make; sudo make install"
 rm -f ${MIDGARD_EXT_VERSION}
 
 # Build and install Midgard2 PHP extension
-wget -q https://github.com/midgardproject/midgard-php5/tarball/${MIDGARD_EXT_VERSION}
+wget -q https://github.com/midgardproject/midgard-php5/tarball/${MIDGARD_EXT_VERSION} -O ${MIDGARD_EXT_VERSION}
 tar zxf ${MIDGARD_EXT_VERSION}
 sh -c "cd midgardproject-midgard-php5-*&&php `pyrus get php_dir|tail -1`/pake.php install"
 echo "extension=midgard2.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
