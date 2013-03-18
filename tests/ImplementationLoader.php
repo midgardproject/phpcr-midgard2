ded4b5b5e6f723be57a5fafeef999a3de04733d9
piotras <piotrek.pokora@gmail.com>
2013-03-18 13:32:42 +0100

    Mark transaction tests unsupported

--- tests/ImplementationLoader.php
+++ tests/ImplementationLoader.php
@@ -6,7 +6,7 @@ class ImplementationLoader extends \PHPCR\Test\AbstractLoader
     protected $unsupportedChapters = array(
         // Features we don't support in the Midgard provider
         'Versioning',
-        //'Transactions',
+        'Transactions',
         'PermissionsAndCapabilities',
         'Locking',
         'Import',
